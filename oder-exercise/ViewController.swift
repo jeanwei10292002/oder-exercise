a0f738f20ac4d2fa7b5c92d96fef9c55f278fd96
jeanwei <jeanwei10292002@gmail.com>
2020-07-18 20:39:48 +0800

    Initial Commit

---
+++ oder-exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  oder-exercise
+//
+//  Created by jeanwei on 2020/7/18.
+//  Copyright © 2020 jeanwei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
